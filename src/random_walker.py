2f9a758d3145eb8fee09345fc1a00ca91512cbf9
hussainsr <hussainsr97@gmail.com>
2020-11-08 00:24:34 -0500

    Added tmap

--- src/random_walker.py
+++ src/random_walker.py
@@ -40,14 +40,17 @@ def rand_b(T): # n is number of steps. T is temp
 	else:
 		return rand_b(T)
 
-def rand_walker_data(n,T,n_steps,num_walkers): # n=number of walkers, T= temperature
+def rand_walker_data(n,T,n_steps,num_walkers,positions): # n=number of walkers, T= temperature, z=positions of seed
 
+	tmap = temp_map(n,T,positions) 
 	x = [0,n] # sets range for x
 	y = [0,n] # sets range for y
 	wlk = [] # init array of walkers
 	for i in range(int(num_walkers**0.5)):
 		for j in range(int(num_walkers**0.5)):
 			wlk.append(walker((i+1)*(max(x)-min(x))/(num_walkers**0.5+1),(j+1)*(max(y)-min(y))/(num_walkers**0.5+1)))
+	for i in range(num_walkers):
+		wlk.append(walker(positions[i,0],positions[i,1]))
 	pos_x = []
 	pos_y = []
 	vel = []
@@ -57,32 +60,48 @@ def rand_walker_data(n,T,n_steps,num_walkers): # n=number of walkers, T= tempera
 		temp_y = np.zeros(num_walkers)
 		temp_z = np.zeros(num_walkers)
 		for j in range(num_walkers):
-			wlk[j].take_step(rand_b(T[j]),x,y)
 			temp_x[j] = wlk[j].x
 			temp_y[j] = wlk[j].y
 			temp_z[j] = wlk[j].v
+			wlk[j].take_step(rand_b(tmap[int(temp_x[j]),int(temp_y[j])]),x,y)
 		pos_x.append(temp_x)
 		pos_y.append(temp_y)
 		vel.append(temp_z)	
 	pos_x = np.asarray(pos_x)
 	pos_y = np.asarray(pos_y)
 	vel   = np.asarray(vel)
-	return pos_x, pos_y, vel
+	return pos_x, pos_y, vel, tmap
 
-def temp_map(n):
-	return np.zeros((n,n))
+def temp_map(n,T,positions):
+	tmap = np.ones((n,n))*(-1.)
+	for i in range(len(positions)):
+		tmap[int(positions[i,0]),int(positions[i,1])] = T[i]
+	for i in range(n):
+		for j in range(n):
+			if tmap[i,j] == -1:
+				tmap[i,j] = 0.0
+				for k in range(len(positions)):
+					x = np.sqrt((positions[k,0]-i)**2+(positions[k,1]-j)**2)
+					tmap[i,j] += T[k]*np.exp(-(x/25)**2)
+#	plt.imshow(tmap)
+	return tmap
 
 #----------------------------------------------------------------------------
-	
-num_walkers = 16
+'''	
+num_walkers = 9
 num_steps = 1000
 n_grid = 100
-T = np.linspace(100,1000,num_walkers) # Temperature
-tmap = temp_map(n_grid) 
-x,y,v = rand_walker_data(n_grid,T,num_steps,num_walkers)
 
+T = np.linspace(250,350,num_walkers) # Temperature
+positions = []
 for i in range(num_walkers):
-	plt.plot(x[:,i],y[:,i],'.')
-plt.show() 
+	positions.append([int(np.random.rand()*100),int(np.random.rand()*100)])
+positions = np.asanyarray(positions)
 
+x,y,v,tmap = rand_walker_data(n_grid,T,num_steps,num_walkers,positions)
+
+#for i in range(num_walkers):
+#	plt.plot(x[:,i],y[:,i],'.')
+#plt.show() 
+'''
 #-------------------------------------------------------------------------
