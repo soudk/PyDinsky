e10866b322c4da2a201bf80c1b32eaf2540ebad9
mkshaw <52977155+mkshaw@users.noreply.github.com>
2020-11-08 00:29:29 +0000

    Changes get_temperature.py to take two arguments: city, and country.

--- src/get_temperature.py
+++ src/get_temperature.py
@@ -8,23 +8,35 @@ Return the temperature of Montreal at a given time. Future versions: take city n
 import requests
 import json
 import os.path as osp
+import argparse
+import country_converter
 
-def get_temperature():
+script_dir = osp.dirname(__file__)
 
-    # Montreal city code is: 6077243
-    montreal = 6077243
+def get_temperature(city, country):
+
     api_key = "1c7b67b1e9de33125b6801dc38c28efc"
 
-    data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id={montreal}&appid={api_key}&units=metric')
+    # convert country code to ISO2 standard (required by API)
+    country_ISO = country_converter.convert(country, to='ISO2')
+
+    # get city id
+    city_id = get_city_id(city, country_ISO)
+
+    # pull temperature, given city_id
+    data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={api_key}&units=metric')
     contents = data.json()
     temperature = contents['main']['temp']
 
     return temperature
 
-# this is how I would get the city name, in future. They have multiple entries for the same cities, would need country code from dropdown too
-"""
-def get_city(city_name):
+def get_city_id(city, country):
 
-    cities = osp.join(osp.dirname, '..', 'data', city_list.json)
-    list(filter(lambda city: city['name'] == 'Montréal', cities))
-"""
+    # pull city listing from data directory
+    cities = json.load(open(osp.join(script_dir, '..', 'data', 'city_list.json'), "r"))
+    # filter city to listings of that city and country, in case more than 1 just take the first entry
+    city_listing = list(filter(lambda entry: entry['name'] == city and entry['country'] == country, cities))[0]
+
+    # return city id
+    return city_listing['id']
+
