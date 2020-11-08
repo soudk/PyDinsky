e28c55055970d7b1015c501a41edf69bb51d92a2
hussainsr <hussainsr97@gmail.com>
2020-11-07 19:16:27 -0500

    Update: Added feature to contain walkers

--- random_walker.py
+++ random_walker.py
@@ -10,13 +10,17 @@ class walker: # Input is boltz rand. variable
 		self.x = x
 		self.y = y
 		self.v = 0.0
-	def take_step(self,v):	
+	def take_step(self,v,xx,yy):	
 		self.v = v
 		t = np.random.rand()*2*np.pi
 		x_step = np.cos(t)
 		y_step = np.sin(t)
 		self.x += v*x_step
+		if self.x < xx[0] or self.x > xx[1]:
+			self.x -= 2*v*x_step		
 		self.y += v*y_step
+		if self.y < yy[0] or self.y > yy[1]:
+			self.y -= 2*v*y_step		
 	def sample(self):
 		return [self.x,self.y,self.v]
 #	def intx	
@@ -24,11 +28,11 @@ class walker: # Input is boltz rand. variable
 def rand_b(T): # n is number of steps. T is temp
 
 	#c = 4.8*10**(-4) # m/k_b for He
-        c = 3.3693*10**(-3) #m/k_b for N2
+	c = 3.3693*10**(-3) #m/k_b for N2
 	def boltz(v,T):
 		return (c*v/T)*np.exp(-c*v*v/2*T)
 		
-	v_mp = np.sqrt(2*c*T)
+	v_mp = np.sqrt(2*c*T) # most probable velocity
 	x = np.random.rand()*v_mp*4. # setting max v value as 4 times most probable
 	y = np.random.rand()*1.2*boltz(v_mp,T) # setting max y limts as just little above the max of pdf
 	if boltz(x,T)>y:
@@ -53,7 +57,7 @@ def rand_walker_data(n,T,n_steps): # n=number of walkers, T= temperature
 		temp_y = np.zeros(num_walkers)
 		temp_z = np.zeros(num_walkers)
 		for j in range(num_walkers):
-			wlk[j].take_step(rand_b(T[j]))
+			wlk[j].take_step(rand_b(T[j]),x,y)
 			temp_x[j] = wlk[j].x
 			temp_y[j] = wlk[j].y
 			temp_z[j] = wlk[j].v
@@ -76,5 +80,4 @@ for i in range(num_walkers):
 	plt.plot(x[:,i],y[:,i],'.')
 plt.show() 
 
-#----------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------
